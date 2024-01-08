b675a0f74cb8cdf545c4d3da1210f532cf7dbee1
kimminam <sdh220405@sdh.hs.kr>
2024-01-08 23:00:13 +0900

    studentPage

--- fuas/src/app/components/Remaining_number_of_people.js
+++ fuas/src/app/components/Remaining_number_of_people.js
@@ -7,7 +7,11 @@ function Remaining_number_of_people(props) {
     <div className="remaining_number_of_people">
       <div className="member_wrap">
         <p>남은인원</p>
-        <p>{remaingNumber}</p>
+          {remaingNumber.map((person) => (
+            <p key={person.teacher_email}>
+              {person.id}명
+            </p>
+          ))}
       </div>
       <div className="move_to_section_wrap">
         <button className="move_to_section">공지방</button>
