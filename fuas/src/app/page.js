6e4fd3d31778f1c034b2b1084d834e2f2efe3817
gimsieun67 <gimsieun67>
2024-01-08 10:07:11 +0900

    커밋1

--- fuas/src/app/page.js
+++ fuas/src/app/page.js
@@ -1,6 +1,6 @@
-'use client';
-import { useEffect, useState } from 'react';
-import styles from './page.module.css';
+"use client";
+import { useEffect, useState } from "react";
+import styles from "./page.module.css";
 
 export default function Home() {
   const [data, setData] = useState([]);
@@ -8,17 +8,33 @@ export default function Home() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('/api/route');
+        const response = await fetch("/api/route");
         const result = await response.json();
         setData(result.data);
         console.log(result);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
       }
     };
 
     fetchData();
   }, []);
 
-  return <main className={styles.main}></main>;
+  return (
+    <>
+      <div className="bg"> </div>
+      <ul className="user">
+        <li>
+          <p>당신은 어떤</p>
+          <p>사용자인가요?</p>
+          <button>
+            <b>Teacher</b>
+          </button>
+          <button>
+            <b>Student</b>
+          </button>
+        </li>
+      </ul>
+    </>
+  );
 }
