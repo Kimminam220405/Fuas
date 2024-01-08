b1eb5d00169c11cfcce7ca2342f759a3f9ea01c0
kimminam <bluemind8520@gmail.com>
2024-01-08 16:16:21 +0900

    studentPage

--- fuas/src/app/studentpage/page.js
+++ fuas/src/app/studentpage/page.js
@@ -1,17 +1,14 @@
 import React from "react";
-import Image from "next/image";
 import styles from "../page.module.css";
-import backImg from "/public/Vector.png";
-import ActionBar from "../components/actionBar";
 import "../static/student.css";
+import ActionBar from "../components/actionBar";
+import Remaining_number_of_people from "../components/Remaining_number_of_people";
 
 export default function Home() {
   return (
     <div className={styles.main}>
-      <div className="backButton">
-        <Image src={backImg} alt="뒤로가기" />
-        <ActionBar pageTitle="신청방" />
-      </div>
+      <ActionBar pageTitle="신청방" />
+      <Remaining_number_of_people />
     </div>
   );
 }
