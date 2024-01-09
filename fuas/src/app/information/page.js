dbaa23531db5d331699edfd5ee5deeb869d35887
gimsieun67 <gimsieun67>
2024-01-09 14:04:39 +0900

    커밋3

--- fuas/src/app/information/page.js
+++ fuas/src/app/information/page.js
@@ -1,8 +1,30 @@
 import styles from "./page.module.css";
+const modalb = document.querySelector(".b");
+const dialog = document.querySelector(".dia");
+modalb.addEventListener("click", () => {});
 
 export default function information() {
+  xx;
   return (
     <>
+      <dialog className={styles.dia}>
+        <ul>
+          <b>이용규칙</b>
+          <li>하루 최대 사용인원은 20명으로 하여</li>
+          <li>순번이 20번이 되면 그날</li>
+          <li>사용 인원 마감이 됩니다.</li>
+
+          <li>AI 융합실은 음식물(음료포함)</li>
+          <li>반입이 절대 금지입니다.</li>
+
+          <li>마지막에 나가는 학생은 정리가 깨끗이 정리가 된</li>
+          <li>AI융합실 전체 사진을 찍어</li>
+          <li>올리고 퇴실합니다.</li>
+          <li>우리모두 깨끗하게 함께</li>
+          <li>사용할 수 있도록 합시다.</li>
+        </ul>
+        <button>확인</button>
+      </dialog>
       <div className={styles.bg}></div>
       <div className={styles.All}>
         <ul className={styles.people}>
